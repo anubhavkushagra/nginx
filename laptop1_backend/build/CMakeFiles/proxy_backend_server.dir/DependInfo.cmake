dddec83c2bcde560f1de4d50b05ebe02878a16ba
Anubhav <anubhavkushagra@github.com>
2026-03-11 09:49:11 +0530

    Change backend ports to 50061-50068 to bypass Windows WSL bindings

---
+++ laptop1_backend/build/CMakeFiles/proxy_backend_server.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/anubhav/kv_server/Nginx/laptop1_backend/generated/kv.grpc.pb.cc" "CMakeFiles/proxy_backend_server.dir/generated/kv.grpc.pb.cc.o" "gcc" "CMakeFiles/proxy_backend_server.dir/generated/kv.grpc.pb.cc.o.d"
+  "/home/anubhav/kv_server/Nginx/laptop1_backend/generated/kv.pb.cc" "CMakeFiles/proxy_backend_server.dir/generated/kv.pb.cc.o" "gcc" "CMakeFiles/proxy_backend_server.dir/generated/kv.pb.cc.o.d"
+  "/home/anubhav/kv_server/Nginx/laptop1_backend/proxy_backend_server.cpp" "CMakeFiles/proxy_backend_server.dir/proxy_backend_server.cpp.o" "gcc" "CMakeFiles/proxy_backend_server.dir/proxy_backend_server.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ laptop1_backend/build/CMakeFiles/proxy_backend_server.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/proxy_backend_server.dir/generated/kv.grpc.pb.cc.o"
+  "CMakeFiles/proxy_backend_server.dir/generated/kv.grpc.pb.cc.o.d"
+  "CMakeFiles/proxy_backend_server.dir/generated/kv.pb.cc.o"
+  "CMakeFiles/proxy_backend_server.dir/generated/kv.pb.cc.o.d"
+  "CMakeFiles/proxy_backend_server.dir/proxy_backend_server.cpp.o"
+  "CMakeFiles/proxy_backend_server.dir/proxy_backend_server.cpp.o.d"
+  "proxy_backend_server"
+  "proxy_backend_server.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/proxy_backend_server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
